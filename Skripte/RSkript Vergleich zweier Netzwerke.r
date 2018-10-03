f25411f24451f0864004860e30f54b6c8386f328
Swaran Sandhu <sandhu@hdm-stuttgart.de>
2018-10-03 13:28:43 +0200

    Update RSkript Vergleich zweier Netzwerke.r

--- Skripte/RSkript Vergleich zweier Netzwerke.r
+++ Skripte/RSkript Vergleich zweier Netzwerke.r
@@ -1,9 +1,9 @@
 # Vergleich zweier Netzwerke mit einfacher Visualisierung
-# Skript für Lehrzwecke in der Veranstaltung 226305 Netzwerk- und Beziehungsmanagement
+## erstellt von sandhu@hdm-stuttgart.de
+## Die Erklärung des Datensatzes und der Codierung finden Sie unter https://github.com/hdm-crpr/226305/blob/master/data/students/codierung.rmd
 
-# Autor: Swaran Sandhu, sandhu@hdm-stuttgart.de
 # inspiriert von https://github.com/kateto/R-Network-Visualization-Workshop
-# Hintergrund: Grundlage sind zwei Netzwerke studentische Netzwerke mit n=38 Teilnehmern mit zwei unterschiedlich gewichteten Beziehungen (3 = starke Beziehung, erste Nennung, 1 = Beziehung). Gefragt wurde nach jeweils zwei Nennungen der Personen, mit denen die Person am liebsten zusammenarbeitet (Netzwerk "work") und wen die Person um Rat fragt (Netzwerk "help). Es konnten maximal zwei Personen aus der gleichen Semesterstufe angegeben werden. Die erste Nennung gilt immer als die stärkere Beziehung. 
+# Hintergrund: Grundlage sind zwei Netzwerke studentische Netzwerke mit n=38 Teilnehmern mit zwei unterschiedlich gewichteten Beziehungen (3 = starke Beziehung, erste Nennung, 1 = Beziehung). Gefragt wurde nach jeweils zwei Nennungen der Personen, mit denen die Person am liebsten zusammenarbeitet (Netzwerk "work") und wen die Person um Rat fragt (Netzwerk "help). Es konnten maximal zwei Personen aus der gleichen Semesterstufe angegeben werden. Die erste Nennung gilt immer als die stärkere Beziehung.
 
 # Variante 1: Laden Sie die Dateien "e-work.csv" und "e-help.csv" in ihre working directory.
 # Variante 2: Dateien können direkt über github eingelesen werden
@@ -34,27 +34,31 @@ help <- graph_from_data_frame(ehelp, directed = TRUE, vertices = NULL)
 
 # addiert die Gewichte bei gleichen Beziehungen
 works <- simplify(work, edge.attr.comb = list(weight="sum"))
-helps <- simplify(ehelp, edge.attr.comb = list(weight="sum"))
+helps <- simplify(help, edge.attr.comb = list(weight="sum"))
 
 # Visualisierung works (orange)
-deg <- degree(works) 
-V(works)$size <- deg*3 
+deg <- degree(works)
+V(works)$size <- deg*3
 V(works)$color <- "orange"
-E(works)$width <- E(works)$weight*1.5 
+E(works)$width <- E(works)$weight*1.5
 E(works)$color <- "orange"
-E(works)$arrow.size <- .4
+E(works)$arrow.size <- .2
 
 # Visualisierung help (blue)
-deg <- degree(helps) 
+deg <- degree(helps)
 V(helps)$size <- deg*3
 V(helps)$color <- "lightblue"
-E(helps)$width <- E(helps)$weight*1.5 
+E(helps)$width <- E(helps)$weight*1.5
 E(helps)$color <- "lightblue"
-E(helps)$arrow.size <- .4
+E(helps)$arrow.size <- .2
 
-# vergleicht in der Zeile, wie der gleiche Algoithmus zu leicht unterschiedlichen Darstellungen kommt.
-par(mfrow=c(2,2), mar=c(0,0,0,0))   
-plot(works, layout=layout_with_fr, main="Projektarbeit")
-plot(helps, layout=layout_with_fr, main="Unterstützung")
-plot(works, layout=layout_with_gem, main="Projektarbeit")
-plot(helps, layout=layout_with_gem, main="Unterstützung")
+# vergleicht in der Zeile, wie der gleiche Algorithmus zu leicht unterschiedlichen Darstellungen kommt.
+
+# legt fest, dass ein Blatt auf 2x3 Felder aufgeteilt werden soll
+par(mfrow=c(2,2), mar=c(0,0,0,0))
+
+# visualisiert die Graphen
+plot(works, layout=layout_with_fr, main="Projektarbeit FR")
+plot(helps, layout=layout_with_fr, main="Unterstützung FR")
+plot(works, layout=layout_with_gem, main="Projektarbeit GEM")
+plot(helps, layout=layout_with_gem, main="Unterstützung GEM")
